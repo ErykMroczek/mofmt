8027393c0de75368d49d70a1cd36362f77f11ed4
Eryk Mroczek <mroczek.eryk@gmail.com>
2023-06-28 15:25:48 +0200

    Create unit tests for Collector class

--- tests/unit/test_collector.py
+++ tests/unit/test_collector.py
@@ -6,7 +6,7 @@ from mofmt.collecting.collector import Collector, Marker
 def test_tokens():
     """Check tokens collecting"""
     col = Collector()
-    val_1, val_2 = "TEST1", "TEST2"
+    val_1, val_2 = "TOK1", "TOK2"
     col.add_token(val_1)
     col.add_token(val_2)
     # Test if number of markers is correct
@@ -18,3 +18,72 @@ def test_tokens():
     assert col.list[0].val == val_1
     assert col.list[-1].val == val_2
     assert col.list[0].val == col.list[0].rep
+
+
+def test_comments():
+    """Check comments collecting"""
+    col = Collector()
+    val_1, val_2 = "COM1", "COM2"
+    col.add_comment(val_1)
+    col.add_comment(val_2)
+    # Test if number of markers is correct
+    assert len(col.list) == 2
+    # Test if markers has a correct type
+    for m in col.list:
+        assert m.typ == Marker.COMMENT
+    # Test markers values and representations
+    assert col.list[0].val == val_1
+    assert col.list[-1].val == val_2
+    assert col.list[0].val == col.list[0].rep
+
+
+def test_tail_caching():
+    """Check collector tail caching that is used in comments handling"""
+    col = Collector()
+    col.add_token("tok")
+    col.add_space()
+    col.add_indent()
+    col.add_linebreak()
+    col.add_space()
+    tail = col.cache_tail()
+    # There should be just indent and break
+    assert len(tail) == 2
+    assert tail[0].typ == Marker.INDENT
+    assert tail[1].typ == Marker.LINEBREAK
+    # Only single token should be left in collector
+    assert len(col.list) == 1
+    assert col.list[0].typ == Marker.TOKEN
+
+
+def test_space():
+    """Check space collecting"""
+    col = Collector()
+    # Initial spaces should be discarded
+    col.add_space()
+    col.add_space()
+    assert len(col.list) == 0
+    # But should be accepted when following a token or comment
+    col.add_token("tok")
+    col.add_space()
+    assert col.list[-1].typ == Marker.SPACE
+    # But they should be discarded while following ignore marker or
+    # newline
+    col.add_ignore()
+    col.add_space()
+    assert col.list[-1].typ == Marker.IGNORE
+    col.add_linebreak()
+    col.add_space()
+    assert col.list[-1].typ == Marker.LINEBREAK
+
+
+def test_blank():
+    """Check blanks collecting"""
+    col = Collector()
+    col.add_token("tok")
+    col.add_linebreak()
+    col.wrapped = True
+    col.add_blank()
+    # Blank should overwrite previous newline marker and add dedent when
+    # expression was wrapped
+    assert len(col.list) == 3
+    assert col.list[1].typ == Marker.DEDENT
