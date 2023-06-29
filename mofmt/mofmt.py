d9a682f1918fc3c990ba597abf14dcca920ec3c5
Eryk Mroczek <mroczek.eryk@gmail.com>
2023-06-29 14:50:35 +0200

    Simplify format_files() in main module

--- mofmt/mofmt.py
+++ mofmt/mofmt.py
@@ -1,11 +1,10 @@
-import operator
 import sys
-from functools import reduce
+from itertools import chain
 from pathlib import Path
 
 from mofmt.io import get_files_from_dir, read_file, write_file
 from mofmt.parsing import parse_source
-from mofmt.printing import pretty_print
+from mofmt.printing import Printer
 
 
 class ParsingError(Exception):
@@ -16,7 +15,7 @@ class ParsingError(Exception):
         super().__init__(self.message)
 
 
-def main():
+def main() -> None:
     format_files(sys.argv)
 
 
@@ -25,15 +24,18 @@ def format_files(args: list[str]) -> None:
     if len(args) < 2:
         raise SystemExit("mofmt takes at least one argument (file/directory path)")
     paths = [Path(arg) for arg in args[1:]]
-    modelica_files = [get_files_from_dir(p) if p.is_dir() else [p] for p in paths]
-    modelica_files = reduce(operator.concat, modelica_files)
+    modelica_files = list(
+        chain.from_iterable(
+            (get_files_from_dir(p) if p.is_dir() else [p] for p in paths)
+        )
+    )
     for file in modelica_files:
         contents = read_file(file)
         try:
             parsed = parse_source(contents)
         except Exception as e:
             raise ParsingError(file) from e
-        fmt = pretty_print(parsed)
+        fmt = Printer(parsed).pretty_print()
         write_file(file, fmt)
 
 
