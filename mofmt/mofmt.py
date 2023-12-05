af201af2082f8942760c90ebbce8aed89624f38b
Eryk Mroczek <mroczek.eryk@gmail.com>
2023-12-05 15:41:43 +0100

    Create help message
    Add -h and -v options to CLI

--- mofmt/mofmt.py
+++ mofmt/mofmt.py
@@ -2,9 +2,20 @@ import sys
 from itertools import chain
 from pathlib import Path
 
+from mofmt import __version__
 from mofmt.io import get_files_from_dir, read_file, write_file
 from mofmt.parsing import parse_source
 from mofmt.printing import Printer
+
+HELP_MSG = """
+mofmt: Modelica code formatter
+
+Usage: mofmt SRC ...
+
+Options:
+-h, --help: display this message and exit
+-v, --version: display a version number and exit
+"""
 
 
 def main() -> None:
@@ -14,7 +25,12 @@ def main() -> None:
 def format_files(args: list[str]) -> None:
     """Format files specified in argument list"""
     if len(args) < 2:
-        raise SystemExit("mofmt takes at least one argument (file/directory path)")
+        raise SystemExit(HELP_MSG)
+    else:
+        if args[1] in ("-h", "--help"):
+            raise SystemExit(HELP_MSG)
+        elif args[1] in ("-v", "--version"):
+            raise SystemExit(f"mofmt, {__version__}")
     paths = [Path(arg) for arg in args[1:]]
     modelica_files = list(
         chain.from_iterable(
