9f59bad826b9eb00f6e2dcd5413d830e680d8b46
Eryk Mroczek <mroczek.eryk@gmail.com>
2024-01-11 08:49:53 +0100

    Add support for recursive directory formatting

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,74 @@
 use mofmt::{format, pretty_print};
 use moparse::{lex, parse, SyntaxKind};
-use std::env;
-use std::fs;
-use std::path::Path;
-use std::path::PathBuf;
+use std::{env, fs};
+use std::path::{Path, PathBuf};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let file_path = &args[1];
-    let contents = read_file(file_path);
-    let tokens = lex(&contents);
-    let events = parse(&tokens, SyntaxKind::StoredDefinition);
-    let markers = format(&tokens, &events);
-    let output = pretty_print(&tokens, &markers);
-    write_file(file_path, output);
+    format_files(&args[1..]);
+}
+
+/// Format files specified in the argument list
+fn format_files(args: &[String]) {
+    let mut files = Vec::new();
+    args.into_iter()
+        .map(|s| PathBuf::from(s))
+        .map(|p| {
+            if p.is_dir() {
+                get_files_from_dir(p)
+            } else {
+                vec![p]
+            }
+        })
+        .for_each(|mut v| files.append(&mut v));
+    files.iter().for_each(|p| {
+        let contents = read_file(p);
+        let tokens = lex(&contents);
+        let events = parse(&tokens, SyntaxKind::StoredDefinition);
+        let markers = format(&tokens, &events);
+        let output = pretty_print(&tokens, &markers);
+        write_file(p, output);
+    });
 }
 
 /// Return all Modelica files from the given directory
-fn get_files_from_dir(dir: &Path) -> Vec<PathBuf> {
+fn get_files_from_dir(dir: PathBuf) -> Vec<PathBuf> {
     let mut files = Vec::new();
-    let paths = fs::read_dir(dir)
+    let paths = fs::read_dir(&dir)
         .expect(format!("{}: error reading from a directory", dir.display()).as_str());
-    for item in paths {
-        match item {
-            Ok(entry) => {
-                let path = entry.path();
-                if path.is_file() && path.extension().is_some() {
-                    if path.extension().unwrap() == "mo" {
-                        files.push(path);
-                    }
-                } else if path.is_dir() {
-                    files.append(&mut get_files_from_dir(path.as_path()));
-                }
+    paths
+        .map(|e| e.unwrap().path())
+        .map(|p| {
+            if p.is_dir() {
+                get_files_from_dir(p)
+            } else if is_modelica(p.as_path()) {
+                vec![p]
+            } else {
+                Vec::new()
             }
-            Err(_) => (),
-        }
-    }
+        })
+        .for_each(|mut v| files.append(&mut v));
 
     files
 }
 
-/// Return contents of the Modelica file
-fn read_file(from: &String) -> String {
-    let path = Path::new(&from);
-    let suffix = path
-        .extension()
-        .expect(format!("{}: is not a Modelica file", from).as_str());
-    if suffix != "mo" {
-        panic!("{}: is not a Modelica file", from);
+/// Return `true` if the file is a Modelica file
+fn is_modelica(f: &Path) -> bool {
+    if let Some(suffix) = f.extension() {
+        return suffix == "mo";
     }
+    false
+}
 
-    fs::read_to_string(path).expect(format!("{}: error reading a file", from).as_str())
+/// Return contents of the Modelica file
+fn read_file(from: &Path) -> String {
+    if !is_modelica(from) {
+        panic!("{}: is not a Modelica file", from.display());
+    }
+    fs::read_to_string(from).expect(format!("{}: error reading a file", from.display()).as_str())
 }
 
 /// Write formatted code to a file
-fn write_file(to: &String, code: String) {
-    fs::write(to, code).expect(format!("{}: error writing a file", to).as_str());
+fn write_file(to: &Path, code: String) {
+    fs::write(to, code).expect(format!("{}: error writing a file", to.display()).as_str());
 }
