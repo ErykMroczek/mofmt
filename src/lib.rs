d739c6f9c63a6e5ed614513ce9aa970754cbc595
Eryk Mroczek <mroczek.eryk@gmail.com>
2024-02-05 16:15:56 +0100

    Run formatter on a whole crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
-use moparse::{SyntaxEvent, Token};
-
+use self::formatting::format;
 use self::printing::print;
 use self::tree::build_tree;
-use self::formatting::format;
+use moparse::{SyntaxEvent, Token};
 
 pub fn pretty_print(tokens: Vec<Token>, comments: Vec<Token>, events: Vec<SyntaxEvent>) -> String {
     let tree = build_tree(tokens, events);
