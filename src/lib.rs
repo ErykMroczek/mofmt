18055c2ce3b8e863969ccab3b48e32e1a75569d8
Eryk Mroczek <mroczek.eryk@gmail.com>
2024-03-07 17:03:53 +0100

    Add unit test for expression formatting

--- src/lib.rs
+++ src/lib.rs
@@ -14,3 +14,24 @@ mod formatting;
 mod markers;
 mod printing;
 mod tree;
+
+mod tests {
+
+    use moparse::{parse, ModelicaToken, SyntaxKind};
+    use super::*;
+
+    fn format_code(input: &str, entry: SyntaxKind) -> String {
+        let parsed = parse("", input, entry);
+        assert_eq!(parsed.errors.len(), 0);
+        pretty_print(parsed.tokens, parsed.comments, parsed.events)
+    }
+
+    #[test]
+    fn test_expression_formatting() {
+        let source = "x+2   * (  - y)";
+        let expected = "x + 2 * (-y)";
+        let actual = format_code(source, SyntaxKind::SimpleExpression);
+        assert_eq!(expected, actual, "expected that operators are surrouned with spaces");
+    }
+
+}
